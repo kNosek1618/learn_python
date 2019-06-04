c88828bf988b179530b1cc2301ef1b03e048a98f
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-04 21:19:51 +0200

    next variable

--- variables.py
+++ variables.py
@@ -1,5 +1,7 @@
 character_as = 'Noise'
 character_language = "python"
+character_national = 'Poland'
 
 print (" " + character_as + " has always been curious about new technologies.")
 print ("The " + character_as + " likes to learn " + character_language + " very much.")
+print ("Everybody claims that, " + character_as + " is from " + character_national + ".")
