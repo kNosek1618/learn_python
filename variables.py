01c68489430a35329a2cf55d18467a4a1499beca
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-04 20:52:49 +0200

    simple variable

---
+++ variables.py
@@ -0,0 +1,5 @@
+character_as = 'Noise'
+character_language = "python"
+
+print (" " + character_as + " has always been curious about new technologies.")
+print ("The " + character_as + " likes to learn " + character_language + " very much.")
